4a2610780f7720ecf41b2623af457a3126cf549c
nbenUK <32130399+Ben208N@users.noreply.github.com>
2021-08-16 22:48:25 +0000

    clean up code

--- main.py
+++ main.py
@@ -4,15 +4,6 @@
 # API_KEY = os.environ['API_KEY']
 # bot = telebot.TeleBot(API_KEY)
 
-# @bot.message_handler(commands=['Greet'])
-# def greet(message):
-#   bot.reply_to(message, "Hey! Hows it going?")
-
-# @bot.message_handler(commands=['hello'])
-# def hello(message):
-#   bot.send_message(message.chat.id, "Hello!")
-
-# bot.polling()
 
 import telebot
 import time
